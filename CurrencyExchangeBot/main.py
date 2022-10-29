414c02a5cc1baeabe1e17adb0bac07a4cb4225fe
KorPavel <kor-p@bk.ru>
2022-10-29 17:26:21 +0300

    Create main.py
    Основная программа, где описано поведение бота на сообщения/команды от пользователя. А также описаны проверки на корректность ввода команд.

---
+++ CurrencyExchangeBot/main.py
@@ -0,0 +1,64 @@
+import telebot
+from config import Backyard as by
+from extensions import API, APIException
+
+bot = telebot.TeleBot(by.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, by.TEXT_HELLO)
+
+
+@bot.message_handler(commands=['help', 'помощь', '?'])
+def help(message):
+    bot.send_message(message.chat.id, by.TEXT_HELP)
+
+
+@bot.message_handler(commands=['BOT'])
+def example(message):
+    bot.send_message(message.chat.id, 'Например:\nдоллар рубль 100')
+
+
+@bot.message_handler(commands=['values', 'валюты', '!'])
+def values(message):
+    text = 'Список конвертируемых валют:'
+    for num, key in enumerate(by.KEYS, 1):
+        text += f'\n{num:>3}.  {key:20} {by.KEYS[key][1]:<20}'
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(content_types=['text'])
+def convert(message):
+    try:
+        text_msg = message.text.lower().replace(',', '.')
+        msg_list = text_msg.split(' ')
+        if len(msg_list) != 3:
+            raise APIException('Неверное число аргументов\n     /BOT как надо')
+        quote, base, amount = msg_list
+        if float(amount) < 0:
+            raise APIException('Количество должно быть положительным числом!')
+        if quote == base:
+            raise APIException('Нельзя конвертировать валюту саму в себя!')
+        try:
+            quote in by.KEYS[quote][0]
+        except KeyError:
+            raise APIException(f'Я не знаю валюту "{quote.upper()}".\n'
+                               f'Смотри список допустимых валют /values')
+        try:
+            base in by.KEYS[base][0]
+        except KeyError:
+            raise APIException(f'Я не знаю валюту "{base.upper()}".\n'
+                               f'Смотри список допустимых валют /values')
+    except APIException as error:
+        bot.reply_to(message, f'Ошибка:\n{error}')
+    except ValueError:
+        bot.reply_to(message, f'Неправильный ввод данных')
+    else:
+        rate, sum_money = API.get_price(base, quote, amount)
+        text = f'По состоянию на сегодня, {by.date},\nкурс составил:  {rate} ' \
+               f'{by.KEYS[base][0].lower()}\n{"_" * 40}\n{amount} {by.KEYS[quote][0].lower()}' \
+               f' = {round(float(sum_money), 2)} {by.KEYS[base][0].lower()}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling(none_stop=True)
